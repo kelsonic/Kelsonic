54e9b0d8749a31ea3935fd5b1e1f51ef4fba76cd
kelsonic <kgadams93@gmail.com>
2016-06-30 22:01:14 -0700

    Add categorize_skills helper method

--- app/helpers/skills_helper.rb
+++ app/helpers/skills_helper.rb
@@ -28,4 +28,31 @@ module SkillsHelper
 
   end
 
+
+
+  def categorize_skills
+    @languages, @frameworks, @libraries, @databases, @other = [], [], [], [], []
+
+    Skill.all.each do |skill| 
+      p skill
+
+      if skill.skill_type == "Languages"
+        @languages.push(skill)
+
+      elsif skill.skill_type == "Frameworks"
+        @frameworks.push(skill)
+
+      elsif skill.skill_type == "Libraries"
+        @libraries.push(skill)
+
+      elsif skill.skill_type == "Databases"
+        @databases.push(skill)
+
+      else
+        @other.push(skill)
+        
+      end
+    end
+  end
+
 end
