8de1b626aaafd8916e09d9896f38bbab311d3e3c
kelsonic <kgadams93@gmail.com>
2016-06-30 22:00:58 -0700

    Refactor skills controller with skills helper

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,30 +1,9 @@
 class SkillsController < ApplicationController
 
+  include SkillsHelper
+
   def index
-
-    @languages, @frameworks, @libraries, @databases, @other = [], [], [], [], []
-
-    Skill.all.each do |skill| 
-      p skill
-
-      if skill.skill_type == "Languages"
-        @languages.push(skill)
-
-      elsif skill.skill_type == "Frameworks"
-        @frameworks.push(skill)
-
-      elsif skill.skill_type == "Libraries"
-        @libraries.push(skill)
-
-      elsif skill.skill_type == "Databases"
-        @databases.push(skill)
-
-      else
-        @other.push(skill)
-        
-      end
-    end
-    
+    categorize_skills
   end
 
 end
