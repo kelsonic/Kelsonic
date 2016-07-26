9fec5e4daeb53baf6ed96f2c54f9c68b5651f93e
kelsonic <kgadams93@gmail.com>
2016-07-26 12:16:04 +0200

    Update error message for project category attribute validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,6 +8,6 @@ class Project < ActiveRecord::Base
   validates :live_link, length: { in: 5..100 }
   validates :image, presence: true
   validates :category, presence: true, length: {in: 2..60}, format: { with: /\A[a-zA-Z .,]+\z/,
-    message: "only allows letters" }
+    message: "only allows letters, spaces, periods, and commas" }
   
 end
