5d77f53f4de5a02e81451fe8249997ead3fd7d54
kelsonic <kgadams93@gmail.com>
2016-07-27 23:13:48 +0200

    Add filter functionality for projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,22 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.all.reverse
+    if !params[:project_type]
+      @projects = Project.all.reverse
+
+    elsif params[:project_type].downcase == 'javascript'
+      @projects = Project.where(category: ['JavaScript']).reverse
+
+    elsif params[:project_type].downcase == 'ruby on rails'
+      @projects = Project.where(category: ['Ruby on Rails']).reverse
+
+    elsif params[:project_type].downcase == 'unity'
+      @projects = Project.where(category: ['Unity']).reverse
+
+    else
+      @projects = Project.all.reverse
+
+    end
   end
   
 end
