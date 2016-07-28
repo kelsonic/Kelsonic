21080c2327880810f518c37009b341b917f05f51
kelsonic <kgadams93@gmail.com>
2016-07-28 11:43:59 +0200

    Refactor assign_projects helper method

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,19 @@
 module ProjectsHelper
+
+  def assign_projects(project_type)
+    if project_type == 'JavaScript'
+      @projects = Project.where("category LIKE ?", '%js%').reverse
+
+    elsif project_type == 'Ruby on Rails'
+      @projects = Project.where("category LIKE ?", '%Rails%').reverse
+
+    elsif project_type == 'Unity3D'
+      @projects = Project.where("category LIKE ?", '%Unity%').reverse
+
+    else
+      @projects = Project.all.reverse
+
+    end
+  end
+
 end
