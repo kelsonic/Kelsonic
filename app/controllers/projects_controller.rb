799311ce00492c23ec3ee194a4f299788534b236
kelsonic <kgadams93@gmail.com>
2016-07-28 11:43:43 +0200

    Whitelist params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,19 @@
 class ProjectsController < ApplicationController
 
+  include ProjectsHelper
+
   def index
-    if !params[:project_type]
-      @projects = Project.all.reverse
-
-    elsif params[:project_type] == 'JavaScript'
-      @projects = Project.where("category LIKE ?", '%js%').reverse
-
-    elsif params[:project_type] == 'Ruby on Rails'
-      @projects = Project.where("category LIKE ?", '%Rails%').reverse
-
-    elsif params[:project_type] == 'Unity3D'
-      @projects = Project.where("category LIKE ?", '%Unity%').reverse
-
+    if params[:project_type]
+      assign_projects(project_params)
     else
       @projects = Project.all.reverse
-
     end
   end
+
+  private
+
+    def project_params
+      params.require(:project_type)
+    end
   
 end
