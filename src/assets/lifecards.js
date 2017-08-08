5ded6d16b30671cfe7757a12a23ffc4a59d24a4b
kelsonic <kgadams93@gmail.com>
2017-08-07 20:20:12 -0400

    Add lifecards

---
+++ src/assets/lifecards.js
@@ -0,0 +1,43 @@
+const LIFECARDS = [
+  // COLORADO
+  {
+    image: 'colorado.jpg',
+    location: 'Colorado',
+    years: '1993-2011',
+    para1: 'I was born and raised in Colorado. In high school, when I wasn\'t skiing the slopes of Vail or Winter Park, I was either studying for my 7 AP classes or working 35+ hours per week at a local hospital.',
+    para2: 'While I loved the nature in Colorado, I deeply wanted to explore and learn about the various perspectives in the world - an education that would require learning outside a classroom.',
+    para3: 'I applied only to one university (an American university in Rome, Italy) and was accepted. So I packed my bags and got ready to leave for the other side of the planet.'
+  },
+
+  // ITALY
+  {
+    image: 'italy.jpg',
+    location: 'Italy',
+    years: '2011-2015',
+    para1: "I moved to Italy right after high school where I attended an American university in Rome. While I traveled all around Italy and 10+ other countries in Europe during my 4 years at the university, my studies were rigorous as I took enough courses to obtain both a Bachelor's of Arts in International Affairs and International Business.",
+    para2: 'While in Italy, I taught myself Italian when socializing with friends there. I also met my Dutch wife in the process, so I decided to learn Dutch as well.',
+    para3: "When I wasn't studying, I created How2Bee.com with my friend from Kenya. Despite the website getting over 12,000 visitors in less than 3 weeks, WordPress proved very frustrating to customize, which pushed me to reprogram the website using Ruby on Rails."
+  },
+
+  // NETHERLANDS
+  {
+    image: 'netherlands.jpg',
+    location: 'Netherlands',
+    years: '2015-2016',
+    para1: 'After I graduated from the American university in Rome, my wife and I moved to the Netherlands. There I worked as the Marketing Manager of a tech startup that teaches kids how to code.',
+    para2: 'As we waited for my wife\'s US Visa, I became interested in programming. At one point, my wife and I both started a Ruby on Rails online bootcamp - and we were immediately hooked.',
+    para3: 'In less than 1.5 months, we created 8 Rails projects and 4 Unity video games. We fell in love with coding. So I decided to refine my skills at Dev Bootcamp in San Francisco.'
+  },
+
+  // SAN FRANCISCO
+  {
+    image: 'sf.jpg',
+    location: 'San Francisco',
+    years: '2016',
+    para1: 'While studying at Dev Bootcamp, there wasn\'t more than 10 hours that went by (including the weekends) that I wasn\'t coding. I pushed myself to build at least one project per week, and it paid off.',
+    para2: 'At the end of the bootcamp, I had built over 20+ projects, most of which use different technologies that I had to learn in only a couple of days.',
+    para3: 'For the final project, I was the team lead to build a self-driving RC car using recurrent neural networks, Raspberry Pis, Python, and many machine learning libraries.'
+  },
+];
+
+export default LIFECARDS;
